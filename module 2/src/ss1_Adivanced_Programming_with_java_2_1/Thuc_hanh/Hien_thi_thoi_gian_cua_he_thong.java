dd6f3032af92ad161ef0d0735322a32e98a818a7
ngoctran97 <91454475+ngoctran97@users.noreply.github.com>
2021-12-01 17:32:21 +0700

    up bai

---
+++ module 2/src/ss1_Adivanced_Programming_with_java_2_1/Thuc_hanh/Hien_thi_thoi_gian_cua_he_thong.java
@@ -0,0 +1,10 @@
+package ss1_Adivanced_Programming_with_java_2_1.Thuc_hanh;
+
+import java.util.Date;
+
+public class Hien_thi_thoi_gian_cua_he_thong {
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println("Npw is " + now);
+    }
+}
