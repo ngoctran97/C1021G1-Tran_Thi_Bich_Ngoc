b1747e41a03eaf2f098c8df9809dcd23bff0b44c
ngoctran97 <91454475+ngoctran97@users.noreply.github.com>
2021-12-15 16:06:38 +0700

    up bai

--- module 2/src/ss11_DSA_Stack_Queue/Exercise/Reverse/Reverse.java
+++ module 2/src/ss11_DSA_Stack_Queue/Exercise/Reverse/Reverse.java
@@ -1,31 +1,32 @@
 package ss11_DSA_Stack_Queue.Exercise.Reverse;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Reverse {
-//    public static void main(String[] args) {
-//        int count, i = 0, j = 0, temp;
-//        int number[] = new int[100];
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Nhap vao so phan tu trong mang : ");
-//        count = scanner.nextInt();
-//        for (i = 0; i < count; i++){
-//            System.out.println("phan tu" + (i+1) + ": ");
-//            number[i] = scanner.nextInt();
-//        }
-//        j = i - 1;
-//        i = 0;
-//        scanner.close();
-//        while (i< j){
-//            temp = number[i];
-//            number[i] = number[j];
-//            number[j] = temp;
-//            i++;
-//            j++;
-//        }
-//        System.out.println("mangr sau khi dao nguoc");
-//        for (i = 0; i < count; i++){
-//            System.out.println(number[i] + " ");
-//        }
-//    }
+    public static void main(String[] args) {
+        int count, i = 0, j = 0, temp;
+        int number[] = new int[100];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap vao so phan tu trong mang : ");
+        count = scanner.nextInt();
+        for (i = 0; i < count; i++){
+            System.out.println("phan tu" + (i+1) + ": ");
+            number[i] = scanner.nextInt();
+        }
+        j = i - 1;
+        i = 0;
+        scanner.close();
+        Stack<Integer> stack = new Stack<>();
+        for (i = 0; i < count; i++){
+            stack.push(number[i]);
+        }
+        for (i = 0; i < count; i++){
+            number[i] = stack.pop();
+        }
+        System.out.println("mangr sau khi dao nguoc");
+        for (i = 0; i < count; i++){
+            System.out.println(number[i] + " ");
+        }
+    }
 }
